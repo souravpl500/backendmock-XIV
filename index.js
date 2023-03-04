14cee29d88b272f560191459e0d2304f5999715d
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-04 14:43:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { wordRouter } = require("./routes/words.routes");
+const { scoreRouter } = require("./routes/scores.routes");
+const { connection } = require("./config/db");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome Home Page");
+});
+
+app.use("/word", wordRouter);
+app.use("/score", scoreRouter);
+
+app.listen(4500, async () => {
+  try {
+    await connection;
+    console.log("Server has been started on 4500");
+  } catch (err) {
+    console.log(err);
+  }
+});
